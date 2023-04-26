5141ef02b8edcbd8d25105263add47969b370319
Dtikoli <tikolidavid@yahoo.com>
2023-04-26 09:55:36 -0700

    Testing Commit

--- main.c
+++ main.c
@@ -37,10 +37,7 @@ int main(int ac, char **av)
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+	fd = fd + 3;
 
 	if (ac == 2)
 	{
